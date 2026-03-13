c9f97cb8940a84a48dd90ae3d9f7e55ebff1af6a
esteban mauricio calderon <145078739+maocalderon@users.noreply.github.com>
2026-03-13 16:50:01 -0500

    Delete proyectos directory

--- proyectos/Programacion Web/U1_Post1_02220132006_Calderon_Mauricio/src/refactored/NotificationService.java
+++
@@ -1,5 +0,0 @@
-package refactored;
-
-public interface NotificationService {
-    void notify(String recipient, String message);
-}
--- proyectos/Programacion Web/U1_Post1_02220132006_Calderon_Mauricio/src/refactored/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package refactored;
-
-import java.util.List;
-
-public interface OrderRepository {
-    void save(Order order);
-    List<Order> findAll();
-    Order findById(String id);
-}
